61bc3c0ea17334d673e4c518458094f9d1ac81b4
chvainickas <38086543+chvainickas@users.noreply.github.com>
2022-09-21 14:10:23 +0100

    Add files via upload
    variables
    concat

---
+++ NameAge.java
@@ -0,0 +1,10 @@
+public class NameAge{
+    public static void main(String[] args){
+        // declare variable
+        int myAge = 26;
+        String myName = "Edward";
+        
+        System.out.println("My name is " + myName );
+        System.out.println("My age is "+ myAge);
+    }
+}
