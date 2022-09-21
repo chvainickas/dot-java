74e39d9bb5bcf7874f8b598c6f0b19caa239d6b8
chvainickas <38086543+chvainickas@users.noreply.github.com>
2022-09-21 14:19:32 +0100

    Add files via upload
    incomplete

---
+++ inputNameAge.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class inputNameAge{
+    public static void main(String[] args){
+        Scanner sc = new Scanner
+        System.out.println("Please enter your name:");
+        String myName = input.nextLine();
+        System.out.println("Please enter your age:");
+        int age = input.nextInt();
+        
+        System.out.println("My name is " + myName + ". Your age is " + age +".");
+    }
+}
