315a5201672e088c8d404859051e25867da4cab8
chvainickas <38086543+chvainickas@users.noreply.github.com>
2022-12-15 21:51:00 +0000

    Update inputNameAge.java

--- inputNameAge.java
+++ inputNameAge.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 public class inputNameAge{
     public static void main(String[] args){
-        Scanner sc = new Scanner
+        Scanner sc = new Scanner(System.in);
         System.out.println("Please enter your name:");
-        String myName = input.nextLine();
+        String myName = sc.nextLine();
         System.out.println("Please enter your age:");
-        int age = input.nextInt();
+        int age = sc.nextInt();
         
-        System.out.println("My name is " + myName + ". Your age is " + age +".");
+        System.out.println("Your name is " + myName + ". Your age is " + age +".");
     }
 }
